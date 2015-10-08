afb1fcdf02cfd7e4bbe0875f4f93459aaceb7810
Jun  Chen <tlnd.sky@gmail.com>
2015-10-08 17:09:41 +1100

    update

--- WebContent/js/pay.js
+++ WebContent/js/pay.js
@@ -1,7 +1,32 @@
+var testOrder = 
+	{
+		"id": 1,
+		"userId": 1,
+		"restaurantId": 1,
+		"deliverId": 1,
+		"totalPrice": 70.0,
+		"deliveryFee": 10.0,
+		"paymentStatus": true,
+		"state": 1,
+		"creatTime": null,
+		"deliverTime": null,
+		"finishTime": null,
+
+		"dishes" : 
+		[
+			{"id": 1, "orderId": 1, "dishId": 1, "amount": 3, "price": 70.0}
+		]
+
+	};
+
 function payInfo() {
     $("#payInfo").append(localStorage.getItem("order"));
 }
 
 function pay() {
 	alert("not implemented yet(:");
+	// send order to server:
+	
+
+	// go to the payment page:
 }
